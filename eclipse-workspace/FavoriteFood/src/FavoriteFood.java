cd7c8503d65893dcf90da80bd0f25fecc6349fdf
LAVIEユーザー <LAVIE@192.168.10.101>
2020-05-17 00:16:40 +0900

    内容を付け加えました。

--- eclipse-workspace/FavoriteFood/src/FavoriteFood.java
+++ eclipse-workspace/FavoriteFood/src/FavoriteFood.java
@@ -3,6 +3,7 @@ public class FavoriteFood {
 	public static void main(String[] args) {
 		System.out.print("私の好きな食べ物はメロンです。");
 		System.out.println("その理由は、甘くておいしいからです。");
+		System.out.println("特に、夏に食べるメロンのパフェがおいしいです。");
 		System.out.println("メロンはウリ科の1年生草本植物である．(Wikipedia調べ)-g17936");
 	}
 }
